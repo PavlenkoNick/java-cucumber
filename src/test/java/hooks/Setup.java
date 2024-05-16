3beb7c9875241cc740ff0c72280ff780d046b57a
Nikolai Pavlenko <pavlenko.nickolay@gmail.com>
2024-05-16 18:57:49 -0400

    Fix: for debugging purpose sets Java logger level to a particular value: finest.

--- src/test/java/hooks/Setup.java
+++ src/test/java/hooks/Setup.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Setup {
     public static WebDriver driver;
 
@@ -19,5 +23,16 @@ public class Setup {
         options.addArguments("--headless=new");
 //        options.addArguments("--window-size=1920,1080");
         this.driver = new ChromeDriver(options);
+    }
+
+    @Before
+    public void setLogsLevel() {
+        // for debugging purpose sets Java logger level to a particular value
+        // Java has 7 logger levels: SEVERE, WARNING, INFO, CONFIG, FINE, FINER, and FINEST. The default is INFO.
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.FINE);
+        Arrays.stream(logger.getHandlers()).forEach(handler -> {
+            handler.setLevel(Level.FINE);
+        });
     }
 }
